58a34392bbe57cce2213498bca45d871ab372e3b
Vegas90 <amosmwoni@gmail.com>
2023-08-17 23:36:42 +0300

    M

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -6,13 +6,13 @@ CREATE TABLE IF NOT EXISTS second_table(
 );
 
 INSERT INTO second_table(int, name, score)
-values(1, 'John', 10)
+values(1, 'John', 10);
 
 INSERT INTO second_table(int, name, score)
-values(2, 'Alex', 3)
+values(2, 'Alex', 3);
 
 INSERT INTO second_table(int, name, score)
-values(3, 'Bob', 14)
+values(3, 'Bob', 14);
 INSERT INTO second_table(int, name, score)
-values(4, 'George', 8)
+values(4, 'George', 8);
 

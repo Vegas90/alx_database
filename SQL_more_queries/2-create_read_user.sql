c8882e0eda339704a2a4b9a6921ec7b68437a9f3
Vegas90 <amosmwoni@gmail.com>
2023-08-21 22:05:32 +0300

    m

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+--CONTENT
+
+CREATE SCHEMA IF NOT EXISTS hbtn_0d_2;
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON *hbtn_0d_2.* TO 'user_0d_`2'@'localhost'; 

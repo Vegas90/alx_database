d351a8776904ae825a8b8c83bc4478e5d9ece6fc
Vegas90 <amosmwoni@gmail.com>
2023-08-21 22:48:35 +0300

    M

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- content
-CREAT TABLE IF NOT EXISTS unique_id(
-id INT DEFAULT 1 UNIQUE,
-name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS unique_id(
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
 );

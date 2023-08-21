92f9ada0c2160dd4cb75ce4144a339965f18e2ff
Vegas90 <amosmwoni@gmail.com>
2023-08-21 22:27:46 +0300

    M

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- content
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEDGES *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEDGES;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;

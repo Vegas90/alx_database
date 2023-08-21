48543a832bcf3a11a0098059bac00fb747d73dfc
Vegas90 <amosmwoni@gmail.com>
2023-08-21 22:55:50 +0300

    M

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- content
 CREATE DATABASE IF NOT EXISTS htbn_0d_usa;
 
-use htbn_0d_usa;
+USE htbn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name VARCHAR(256) NOT NULL

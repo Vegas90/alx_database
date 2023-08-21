faf754d0fe1c320a193ab15354a0b56db75e37fb
Vegas90 <amosmwoni@gmail.com>
2023-08-21 23:18:45 +0300

    M

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- content
 SELECT * FROM hbtn_0d_usa;
-WHERE state.id=(SELECT id FROM states WHERE name = 'California')
+WHERE state(id)=(SELECT id FROM states WHERE name = 'California')
 ORDER BY id ASC;
